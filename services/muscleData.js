291bfb7143d0e36627a9561a814e09502dce6c53
flowsx <kocsisadrian10@gmail.com>
2025-02-20 19:44:50 +0100

    Add muscleGroups data structure for workout categorization

---
+++ services/muscleData.js
@@ -0,0 +1,32 @@
+export const muscleGroups = [
+  {
+    id: "chest",
+    name: "Chest",
+    icon: "weight-lifter",
+  },
+  {
+    id: "back",
+    name: "Back",
+    icon: "human-handsup",
+  },
+  {
+    id: "legs",
+    name: "Legs",
+    icon: "run",
+  },
+  {
+    id: "shoulders",
+    name: "Shoulders",
+    icon: "dumbbell",
+  },
+  {
+    id: "arms",
+    name: "Arms",
+    icon: "arm-flex-outline",
+  },
+  {
+    id: "abs",
+    name: "Abs",
+    icon: "hexagram-outline",
+  },
+];
